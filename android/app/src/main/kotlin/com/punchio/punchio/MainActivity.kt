21d8fd6fb310e7f63bb71ac75030ee9ed6386fce
ijesse96 <jesse.anthony.junior@gmail.com>
2025-09-05 16:37:13 -0700

    Switch to GitHub Actions: remove codemagic.yaml and add iOS build workflow

--- android/app/src/main/kotlin/com/punchio/punchio/MainActivity.kt
+++ android/app/src/main/kotlin/com/punchio/punchio/MainActivity.kt
@@ -2,10 +2,26 @@ package com.punchio.punchio
 
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity : FlutterActivity() {
+    private val CHANNEL = "com.punchio.punchio/audio_monitoring"
+    
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-        flutterEngine.plugins.add(AudioMonitoringPlugin())
+        
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            when (call.method) {
+                "startMonitoring" -> {
+                    AudioMonitoringPlugin().startMonitoring(result)
+                }
+                "stopMonitoring" -> {
+                    AudioMonitoringPlugin().stopMonitoring(result)
+                }
+                else -> {
+                    result.notImplemented()
+                }
+            }
+        }
     }
 }
